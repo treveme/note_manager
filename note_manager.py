558c4532ab955fb3feb6e2f633c99abe0a73d02d
treveme <ry.derty27@gmail.com>
2025-01-10 01:50:20 +0300

    Add files via upload

---
+++ note_manager.py
@@ -0,0 +1,12 @@
+username = 'Александр'
+print(username)
+title = 'Покупка продуктов'
+print(title)
+content = 'Купить горошек, черный хлеб.'
+print(content)
+status = 'active'
+print(status)
+created_date =  "25-12-2024"
+print(created_date)
+issue_date = "10-01-2025"
+print(issue_date)
