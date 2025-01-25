45495ac190f45be547f0c6e59c64c9d0912659b2
treveme <ry.derty27@gmail.com>
2025-01-25 11:33:08 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,10 +1,14 @@
-from datetime import datetime
-
-created_date = datetime(2024, 12, 18)
-issue_date = datetime(2024, 12, 18)
-
-temp_created_date = created_date.strftime("%d-%m")
-temp_issue_date = issue_date.strftime("%d-%m")
-
-print(f"Created Date: {temp_created_date}")
-print(f"Issue Date: {temp_issue_date}")
+username = "Александр"
+title = "Заголовок"
+content = "Заметка"
+status = "Статус заметки"
+created_date = "10-11-2024"
+issue_date = "10-12-2024"
+temp_issue_date = created_date[: 5]
+temp_created_date = issue_date[: 5]
+print("Имя пользователя: ", username)
+print("Заголовок: ", title)
+print("Заметка: ", content)
+print("Статус заметки: ", status)
+print("Дата создания заметки: ", temp_created_date)
+print("Дата истечения заметки (дедлайн): ", temp_issue_date)
