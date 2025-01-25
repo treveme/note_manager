a1862557a502285bed09eac1ea5eeb8a45a93e14
treveme <ry.derty27@gmail.com>
2025-01-25 11:34:52 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,22 +1,37 @@
-note0 = ("Имя пользователя: ","Содержание заметки: ","Статус заметки: ","Дата создания заметки в формате {10-11-2024}: ", "Дата истечения заметки (дедлайн) в формате {10-11-2024}: ","Заголовок: ")
-note = ("username", "content", "status", "created_date", "issue_date", "titles")
-note_dict = dict.fromkeys(note, "Ввод текста")
-note_dict["titles"] = [1,2,3]
-note_dict["username"] = input("Введите "+ note0[0])
-note_dict["content"] = input("Введите "+ note0[1])
-note_dict["status"] = input("Введите "+ note0[2])
-note_dict["created_date"] = input("Введите "+ note0[3])
-note_dict["issue_date"] = input("Введите "+ note0[4])
-note_dict["titles"][0] = input("Введите "+ note0[5]+"1: ")
-note_dict["titles"][1] = input("Введите "+ note0[5]+"2: ")
-note_dict["titles"][2] = input("Введите "+ note0[5]+"3: ")
-temp_created_date = note_dict["created_date"][: 5]
-temp_issue_date = note_dict["issue_date"][: 5]
-print(note0[0],note_dict["username"])
-print(note0[1],note_dict["content"])
-print(note0[2],note_dict["status"])
-print(note0[3][:21]+": ",temp_created_date)
-print(note0[4][:21]+": ",temp_issue_date)
-print(note0[5]+"1: ",note_dict["titles"][0])
-print(note0[5]+"2: ",note_dict["titles"][1])
-print(note0[5]+"3: ",note_dict["titles"][2])
+username = input("Введите имя пользователя: ")
+title = input("Введите заголовок заметки: ")
+content = input("Введите описание заметки: ")
+status = input("Введите статус заметки: ")
+created_date = input("Введите дату создания заметки в формате 'день-месяц-год': ")
+issue_date = input("Введите дату истечения заметки в формате 'день-месяц-год': ")
+
+headings = []
+for i in range(3):
+    heading = input(f"Введите заголовок {i + 1}: ")
+    headings.append(heading)
+
+note = [
+    username,
+    title,
+    content,
+    status,
+    created_date,
+    issue_date,
+    headings 
+]
+
+
+temp_created_date = created_date[0:5]  
+temp_issue_date = issue_date[0:5]      
+
+
+print("\n--- Информация о заметке ---")
+print("Имя пользователя:", note[0])
+print("Заголовок заметки:", note[1])
+print("Описание заметки:", note[2])
+print("Статус заметки:", note[3])
+print("Дата создания заметки (без года):", temp_created_date)
+print("Дата истечения заметки (без года):", temp_issue_date)
+print("Заголовки заметки:")
+for h in note[6]:  
+    print("-", h)
